a94525e005ab9d01eb463944eeb64da1c6f92409
123shang60 <616761475@qq.com>
2022-03-23 21:52:31 +0800

    feat : 更改minio 清理逻辑

--- pkg/minio.go
+++ pkg/minio.go
@@ -28,15 +28,15 @@ func AutoConnMinio(endpoints, accessKeyID, secretAccessKey string) (*MinioClient
 	}, nil
 }
 
-// TracBucket 清理 minio bucket ，达到 flink 可用状态
+// TracFile 清理 minio 中 ha 相关的文件 ，从而达到 flink 可用状态
 // 逻辑如下：
 // 1、如果没有 bucket ，要创建
-// 2、如果有 bucket，要清除全部 object ，然后创建
+// 2、如果有 bucket，要删除指定路径下的 object ，然后创建
 // 2.1、清除逻辑参考 mc 源码 (https://github.com/minio/mc/blob/RELEASE.2022-02-23T03-15-59Z/cmd/rb-main.go#L153) ，批量删除后清除空 bucket
-func (m *MinioClient) TracBucket(name string) error {
-	klog.Info("准备清理 m，bucket : ", name)
+func (m *MinioClient) TracFile(bucket, prefix string) error {
+	klog.Info("准备清理 m，bucket : ", bucket)
 	ctx := context.Background()
-	exists, err := m.BucketExists(ctx, name)
+	exists, err := m.BucketExists(ctx, bucket)
 	if err != nil {
 		klog.Error("m 清理失败！", err)
 		return err
@@ -44,28 +44,29 @@ func (m *MinioClient) TracBucket(name string) error {
 	if exists {
 		klog.Info("bucket 已经存在，开始清理")
 		// TODO: clean all object and bucket
-		if err := m.removeBucket(ctx, name); err != nil {
+		if err := m.removeFile(ctx, bucket, prefix); err != nil {
 			klog.Error("minio bucket 清理失败！", err)
+			return err
+		}
+	} else {
+		if err := m.MakeBucket(ctx, bucket, minio.MakeBucketOptions{}); err != nil {
+			klog.Error("bucket 创建失败！,", err)
 			return err
 		}
 	}
 
-	if err := m.MakeBucket(ctx, name, minio.MakeBucketOptions{}); err != nil {
-		klog.Error("bucket 创建失败！,", err)
-		return err
-	}
 	klog.Info("minio 清理成功!")
 	return nil
 }
 
 // removeBucket 清理 bucket，无论是否为空
-func (m *MinioClient) removeBucket(ctx context.Context, name string) error {
+func (m *MinioClient) removeFile(ctx context.Context, bucket, prefix string) error {
 	objectCh := make(chan minio.ObjectInfo)
 
 	go func() {
 		defer close(objectCh)
-		for object := range m.ListObjects(ctx, name, minio.ListObjectsOptions{
-			Prefix:    "",
+		for object := range m.ListObjects(ctx, bucket, minio.ListObjectsOptions{
+			Prefix:    prefix,
 			Recursive: true,
 		}) {
 			objectCh <- object
@@ -76,16 +77,11 @@ func (m *MinioClient) removeBucket(ctx context.Context, name string) error {
 		GovernanceBypass: true,
 	}
 
-	for err := range m.RemoveObjects(ctx, name, objectCh, opts) {
+	for err := range m.RemoveObjects(ctx, bucket, objectCh, opts) {
 		if err.Err != nil {
 			klog.Error("object 删除失败！", err)
 			return err.Err
 		}
-	}
-
-	if err := m.RemoveBucket(ctx, name); err != nil {
-		klog.Error("minio bucket 删除失败!", err)
-		return err
 	}
 
 	klog.Info("minio 清理成功!")
--- pkg/minio_test.go
+++ pkg/minio_test.go
@@ -20,15 +20,15 @@ func TestCreateBucket(t *testing.T) {
 	conn, err := AutoConnMinio(EndPoint, AccessKeyID, SecretAccessKey)
 	assert.Nil(t, err)
 
-	err = conn.TracBucket(TestBucketCreate)
+	err = conn.TracFile(TestBucketCreate, "")
 	assert.Nil(t, err)
 }
 
-func TestCleanBucket(t *testing.T) {
+func TestCleanFile(t *testing.T) {
 	conn, err := AutoConnMinio(EndPoint, AccessKeyID, SecretAccessKey)
 	assert.Nil(t, err)
 
-	err = conn.TracBucket(TestBucketDelete)
+	err = conn.TracFile(TestBucketDelete, "")
 	assert.Nil(t, err)
 
 	// 上传一些文件
@@ -45,6 +45,30 @@ func TestCleanBucket(t *testing.T) {
 
 	assert.Nil(t, err)
 
-	err = conn.TracBucket(TestBucketDelete)
+	err = conn.TracFile(TestBucketDelete, "")
+	assert.Nil(t, err)
+}
+
+func TestCleanFilePrefix(t *testing.T) {
+	conn, err := AutoConnMinio(EndPoint, AccessKeyID, SecretAccessKey)
+	assert.Nil(t, err)
+
+	err = conn.TracFile(TestBucketDelete, "")
+	assert.Nil(t, err)
+
+	// 上传一些文件
+	reader := strings.NewReader("somethings test")
+
+	_, err = conn.PutObject(
+		context.Background(),
+		TestBucketDelete,
+		"test/deletes.txt",
+		reader,
+		reader.Size(),
+		minio.PutObjectOptions{ContentType: "application/octet-stream"},
+	)
+	assert.Nil(t, err)
+
+	err = conn.TracFile(TestBucketDelete, "test")
 	assert.Nil(t, err)
 }
